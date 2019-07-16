fba90745c2abab385b6e1f0f62070957d73dc841
wil.i.am <wiliam.tae@gmail.com>
2019-07-16 10:22:26 -0300

    sumTwolower kata

---
+++ sumTwoLower.js
@@ -0,0 +1,32 @@
+function sumTwoSmallestNumbers(numbers) {  
+    //Code here
+
+    let num = numbers.map(elem => {
+        if (elem >= 0) {
+            return elem
+        }
+    });
+
+
+    let minOne = Math.min(...num)
+    let minTwo = num.length > 1 ? Math.min.apply(null, num.filter( numb => numb != minOne)) : 0
+     return num.length > 0 ? minOne + minTwo : 0
+    }
+
+  console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]) )
+//   13
+  console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]))
+//   6
+  console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7]))
+//   10 
+  console.log(sumTwoSmallestNumbers([23, 71, 33, 82, 1])) 
+//   24
+  console.log(sumTwoSmallestNumbers([]))
+//   16
+  
+
+function sumTwoSmallestNumbers(numbers) {  
+    var [ a, b ] = numbers.sort((a, b) => a - b)
+    console.log([a,b])
+    return a + b
+  }
